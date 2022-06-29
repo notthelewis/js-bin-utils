51a83ca2cc24ded7a608aeca45046798c346d70b
Lewis Farnworht <lewfar99@gmail.com>
2022-06-29 20:11:48 +0100

    Changed the export strategy so that the validate and convert objects are exported differently.

--- src/index.ts
+++ src/index.ts
@@ -15,20 +15,19 @@ import {
     three_u8_to_u32,
 } from './converters';
 
-export default {
-    validate: {
-        nibble: is_valid_nibble,
-        u8: is_valid_u8,
-        u16: is_valid_u16,
-        u32: is_valid_u32
-    },
-    convert_type: {
-        nx2_u8x1: two_nibble_to_one_u8,
-        u8x1_nx2: one_u8_to_two_nibbles,
-        u8x2_u16x1: two_u8_to_one_u16,
-        u8x3_u32x1: three_u8_to_u32,
-        u16x1_u8x2: one_u16_to_two_u8,
-        u16x2_u32x1: two_u16_to_one_u32,
-        u32x1_u16x2: one_u32_to_two_u16,
-    }
+export const validate = {
+    nibble: is_valid_nibble,
+    u8: is_valid_u8,
+    u16: is_valid_u16,
+    u32: is_valid_u32
+};
+
+export const convert = {
+    nx2_u8x1: two_nibble_to_one_u8,
+    u8x1_nx2: one_u8_to_two_nibbles,
+    u8x2_u16x1: two_u8_to_one_u16,
+    u8x3_u32x1: three_u8_to_u32,
+    u16x1_u8x2: one_u16_to_two_u8,
+    u16x2_u32x1: two_u16_to_one_u32,
+    u32x1_u16x2: one_u32_to_two_u16,
 };
