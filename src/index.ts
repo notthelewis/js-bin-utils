c2f336e1c7b4db36e5aca1ef92f2cd429be1268a
Lewis Farnworth <43108320+notthelewis@users.noreply.github.com>
2022-07-01 02:10:52 +0100

    Updated the index to use the new conversion function format.

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,7 @@ import {
     is_valid_u32
 } from './validators';
 
-import {
-    two_nibble_to_one_u8,
-    one_u8_to_two_nibbles,
-    two_u8_to_one_u16,
-    one_u16_to_two_u8,
-    two_u16_to_one_u32,
-    one_u32_to_two_u16,
-    three_u8_to_u32,
-} from './converters';
+import * as convert_fns from './convert_functions/index';
 
 export const validate = {
     nibble: is_valid_nibble,
@@ -22,12 +14,4 @@ export const validate = {
     u32: is_valid_u32
 };
 
-export const convert = {
-    nx2_u8x1: two_nibble_to_one_u8,
-    u8x1_nx2: one_u8_to_two_nibbles,
-    u8x2_u16x1: two_u8_to_one_u16,
-    u8x3_u32x1: three_u8_to_u32,
-    u16x1_u8x2: one_u16_to_two_u8,
-    u16x2_u32x1: two_u16_to_one_u32,
-    u32x1_u16x2: one_u32_to_two_u16,
-};
+export const convert = { ...convert_fns.default };
