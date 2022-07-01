c468ed0bb84d519eb7beafcc586daa02b9dbbe9f
Lewis Farnworht <lewfar99@gmail.com>
2022-07-01 13:45:46 +0100

    Moved the one_u32_to_two_u16s function out of u16.ts and into u32.ts.

--- src/convert_functions/u32.ts
+++ src/convert_functions/u32.ts
@@ -1,6 +1,29 @@
 import { is_valid_u32 } from '../validators';
 
 /**
+  * This function converts a valid, 32-bit unsigned integer into two 16-bit
+  * signed integers.
+  * @example Converting one u32 into two u16 values
+  * ```
+  * const u32 = 0xDEADBEEF;
+  * const [ left, right ] = convert.u32x1_u16x2(u32);
+  * console.log(left.toString(16), right.toString(16));
+  * // [ 0xDEAD, 0xBEEF ]
+  * ```
+  * @param u32 - The 32-bit unsigned integer to convert
+  * @returns An array of two u16s. The left half at index 0, the right at 1
+**/
+export function one_u32_to_two_u16(u32: number): [number, number] {
+    if (! is_valid_u32(u32)) {
+        throw new Error("util::one_u32_to_two_u16::is_valid_u32::false");
+    }
+
+    return [u32 >>> 16, u32 & 0xFFFF];
+}
+
+
+
+/**
   * This function takes one valid u32, then converts it into 4 u8s.
   * @example
   * ```js
