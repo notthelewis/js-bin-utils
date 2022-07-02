0596ad082deeba973e8b454e20b11c08dee329f7
Lewis Farnworth <43108320+notthelewis@users.noreply.github.com>
2022-07-02 02:22:53 +0100

    Exported the DataTypes class, so that Jest can test it. Added documentation to every bit_set function, and cleaned up the param checks inside the functions.

--- src/bit_manipulation_functions/is_bit_set.ts
+++ src/bit_manipulation_functions/is_bit_set.ts
@@ -6,12 +6,14 @@ import {
 } from "../validators";
 
 
-class DataType {
+export class DataType {
     private type_name: string;
     private lower_bound = 0;
     private higher_bound = 7;
 
-    private valid_handler = (val: number): boolean => false;
+    // Tell jest not to include this in coverage...
+    /* c8 ignore next */
+    valid_handler = (val: number): boolean => false;
 
     constructor(type_name: string) {
         switch (type_name) {
@@ -43,8 +45,10 @@ class DataType {
     }
 
     validate(value: number, position: number): boolean {
-        if (isNaN(position) || typeof position != "number") {
-            throw new Error(`util::DataType::${this.type_name}_bit_set::validate::InvalidParameter`);
+        for (const param of arguments) {
+            if(isNaN(param) || typeof param != "number") {
+                throw new Error(`util::DataType::${this.type_name}_bit_set::validate::InvalidParameter`);
+            }
         }
 
         if (! this.valid_handler(value)) return false;
@@ -61,33 +65,103 @@ class DataType {
     }
 }
 
+/**
+  * This function checks whether a bit at a given position in a nibble is set.
+  * @example Checking whether bits are set for value: 2
+  * ```
+  * const value = 0B0010;
+  * console.log(bits.is_bit_set(1)); // true
+  * console.log(bits.is_bit_set(0)); // false
+  * console.log(bits.is_bit_set(4)); // Error, out of bounds
+  *
+  * ```
+  * @param value - A valid nibble, between 0 and 15.
+  * @param position - The position of the bit to check, from zero to 3
+  * @returns boolean
+**/
 export function nibble_bit_set(value: number, position: number): boolean {
-    if (typeof value == undefined || typeof position == undefined) {
-        throw new Error("util::DataType::nibble_bit_set::InvalidParameters");
+    for (const param of arguments) {
+        if(isNaN(param) || typeof param != "number") {
+            throw new Error("util::DataType::nibble_bit_set::InvalidParameters");
+        }
     }
 
     return new DataType('nibble').is_bit_set(value, position);
 }
 
+
+
+/**
+  * This function checks whether a bit at a given position in a u8 is set.
+  * @example Checking whether bits are set in value: 0B10010001
+  * ```
+  * const value = 0B10010001;
+  * console.log(bits.is_bit_set(7)); // true
+  * console.log(bits.is_bit_set(1)); // false
+  * console.log(bits.is_bit_set(8)); // Error, out of bounds
+  *
+  * ```
+  * @param value - A valid u8, between 0 and 255.
+  * @param position - The position of the bit to check, from zero to 7
+  * @returns boolean
+**/
 export function u8_bit_set(value: number, position: number): boolean {
-    if (typeof value == undefined || typeof position == undefined) {
-        throw new Error("util::DataType::u8_bit_set::InvalidParameters");
+    for (const param of arguments) {
+        if(isNaN(param) || typeof param != "number") {
+            throw new Error("util::DataType::u8_bit_set::InvalidParameters");
+        }
     }
 
     return new DataType('u8').is_bit_set(value, position);
 }
 
+
+
+/**
+  * This function checks whether a bit at a given position in a u16 is set.
+  * @example Checking whether bits are set for value: 0b1000100010001000
+  * ```
+  * const value = 0b1000100010001000;
+  * console.log(bits.is_bit_set(15)); // true
+  * console.log(bits.is_bit_set(2);   // false
+  * console.log(bits.is_bit_set(16)); // Error, out of bounds
+  *
+  * ```
+  * @param value - A valid u16 between 0 and 0xFFFF.
+  * @param position - The position of the bit to check, from zero to 15
+  * @returns boolean
+**/
 export function u16_bit_set(value: number, position: number): boolean {
-    if (typeof value == undefined || typeof position == undefined) {
-        throw new Error("util::DataType::u16_bit_set::InvalidParameters");
+    for (const param of arguments) {
+        if(isNaN(param) || typeof param != "number") {
+            throw new Error("util::DataType::u16_bit_set::InvalidParameters");
+        }
     }
 
     return new DataType('u16').is_bit_set(value, position);
 }
 
+
+
+/**
+  * This function checks whether a bit at a given position in a u32 is set.
+  * @example Checking whether bits are set for value: 0b1000100010001000
+  * ```
+  * const value = 0b10001000100010001000100010001000;
+  * console.log(bits.is_bit_set(31)); // true
+  * console.log(bits.is_bit_set(2)); // false
+  * console.log(bits.is_bit_set(32)); // Error, out of bounds
+  *
+  * ```
+  * @param value - A valid u32
+  * @param position - The position of the bit to check, from zero to 31
+  * @returns boolean
+**/
 export function u32_bit_set(value: number, position: number): boolean {
-    if (typeof value == undefined || typeof position == undefined) {
-        throw new Error("util::DataType::u32_bit_set::InvalidParameters");
+    for (const param of arguments) {
+        if(isNaN(param) || typeof param != "number") {
+            throw new Error("util::DataType::u32_bit_set::InvalidParameters");
+        }
     }
 
     return new DataType('u32').is_bit_set(value, position);
