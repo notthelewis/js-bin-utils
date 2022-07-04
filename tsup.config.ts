632d9bc06bac3abfda05a01d0751396af3b470fe
Lewis Farnworht <lewfar99@gmail.com>
2022-07-04 16:18:52 +0100

    Removed the tsc step from compile script.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
     clean: true,
-    entry: ['src/index.ts'],
-    format: ['esm', 'cjs'],
-    env: { 'NODE_ENV': 'production' },
-    dts: true
+    entry: ["src/index.ts"],
+    format: ["esm", "cjs"],
+    env: { NODE_ENV: "production" },
+    dts: true,
+    keepNames: true,
+    minify: true,
+    minifyWhitespace: true,
+    minifySyntax: true,
+    treeshake: "smallest",
 });
